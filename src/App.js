f1df0ea88066f283015d1e57a346188457068365
Patrick Lombardo <patrick_lombardo@comcast.com>
2020-02-05 16:56:44 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BgImage from "./sf.jpg";
 
-function App() {
+const App = () => {
+  const [status, setStatus] = useState("Drop Here");
+  const [preview, setPreview] = useState(null);
+  const [percentage, setPercentage] = useState(0);
+  const [enableDragDrop, setEnableDragDrop] = useState(true);
+
+  const doNothing = event => event.preventDefault();
+
+  const onDragEnter = event => {
+    if (enableDragDrop) {
+      setStatus("File Detected");
+    }
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  const onDragLeave = event => {
+    if (enableDragDrop) {
+      setStatus("Drop Here");
+    }
+    event.preventDefault();
+  };
+
+  const onDragOver = event => {
+    if (enableDragDrop) {
+      setStatus("Drop");
+    }
+    event.preventDefault();
+  };
+
+  const onDrop = event => {
+    const supportedFileTypes = ["image/jpeg", "image/png"];
+    const { type } = event.dataTransfer.files[0];
+    if ((supportedFileTypes.indexOf(type) > -1) && enableDragDrop) {
+      // Read file
+      const reader = new FileReader();
+      reader.onload = e => setPreview(e.target.result);
+      reader.readAsDataURL(event.dataTransfer.files[0]);
+      console.log(reader);
+
+      // Create Form Data
+      const payload = new FormData();
+      payload.append("file", event.dataTransfer.files[0]);
+
+      // XHR - New Request
+      const xhr = new XMLHttpRequest();
+
+      // XHR - Upload Progress
+      xhr.upload.onprogress = e => {
+        const done = e.position || e.loaded;
+        const total = e.totalSize || e.total;
+        const perc = Math.floor((done / total) * 1000) / 10;
+
+        if (perc >= 100) {
+          setStatus("Done");
+          setEnableDragDrop(true);
+        } else {
+          setStatus(`${perc}%`);
+        }
+        setPercentage(perc);
+      };
+
+      // XHR - Make Request
+      xhr.open("POST", "http://localhost:5000/upload");
+      xhr.send(payload);
+
+      setEnableDragDrop(false);
+    }
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      onDragOver={doNothing}
+      onDrop={onDragLeave}
+    >
+      <div className={`ImagePreview ${preview ? "Show" : ""}`}>
+        <div style={{ backgroundImage: `url(${preview})` }}></div>
+      </div>
+      <div
+        className={`DropArea ${status === "Drop" ? "Over" : ""}`}
+        onDragOver={onDragOver}
+        onDragLeave={onDragEnter}
+        onDrop={onDrop}
+      >
+        <div className={`ImageProgress ${preview ? "Show" : ""}`}>
+          <div
+            className="ImageProgressImage"
+            style={{ backgroundImage: `url(${preview})` }}
+          ></div>
+          <div
+            className="ImageProgressUploaded"
+            style={{
+              backgroundImage: `url(${preview})`,
+              clipPath: `inset(${100 - Number(percentage)}% 0 0 0)`
+            }}
+          ></div>
+        </div>
+        <div className="Status">{status}</div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
